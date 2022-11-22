cfdd7e5bde68f622e73d57b8131c7cfd70cc8e49
Tedd Arcuri <tedd.arcuri@gmail.com>
2022-11-21 23:18:40 -0700

    Run PackerSync whenever plugins-setup is modified

--- nvim/lua/saysthetedd/plugins-setup.lua
+++ nvim/lua/saysthetedd/plugins-setup.lua
@@ -1,4 +1,29 @@
-return require('packer').startup(function()
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- Packer Sync whenever this file is saved 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+  augroup end
+]])
+
+local status, packer = pcall(require, "packer")
+if not status then
+  return
+end
+
+return packer.startup(function()
   use 'wbthomason/packer.nvim'
   
   -- themez

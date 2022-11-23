ea6015a7e0964052bced1633c1c3784bd02fc582
Tedd Arcuri <tedd.arcuri@gmail.com>
2022-11-22 20:24:20 -0700

    Update autopair to work with autocomplete

--- nvim/lua/saysthetedd/plugins-setup.lua
+++ nvim/lua/saysthetedd/plugins-setup.lua
@@ -1,16 +1,16 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
--- Packer Sync whenever this file is saved 
+-- Packer Sync whenever this file is saved
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -20,115 +20,115 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
-return packer.startup(function()
-  use 'wbthomason/packer.nvim'
-  
-  -- themez
-  use 'savq/melange'
-  use 'kvrohit/mellow.nvim'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use 'sam4llis/nvim-tundra'
-  use 'sainnhe/everforest'
-  use 'sainnhe/gruvbox-material'
-  use 'sainnhe/sonokai'
+return packer.startup(function(use)
+	use("wbthomason/packer.nvim")
 
-  -- telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    -- or                          , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  } 
-  use { "nvim-telescope/telescope-file-browser.nvim" } -- file tree navigator
-  use { "nvim-telescope/telescope-fzf-native.nvim", run ='make' }
+	-- themez
+	use("savq/melange")
+	use("kvrohit/mellow.nvim")
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("sam4llis/nvim-tundra")
+	use("sainnhe/everforest")
+	use("sainnhe/gruvbox-material")
+	use("sainnhe/sonokai")
 
-  -- autocomplete
-     use 'hrsh7th/nvim-cmp'
-     use 'hrsh7th/cmp-buffer' 
-     use 'hrsh7th/cmp-path'
+	-- telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		-- or                          , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({ "nvim-telescope/telescope-file-browser.nvim" }) -- file tree navigator
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-  -- snippets
-     use 'L3MON4D3/LuaSnip'
-     use 'saadparwaiz1/cmp_luasnip'
-     use 'rafamadriz/friendly-snippets'
+	-- autocomplete
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
 
-  -- LSP
-    use 'williamboman/mason.nvim' --  manage + install
-    use 'williamboman/mason-lspconfig.nvim' 
-    use 'neovim/nvim-lspconfig' -- configure lsp servers
-    use 'hrsh7th/cmp-nvim-lsp' -- autocomplete lsp
-    use { 'glepnir/lspsaga.nvim', branch = 'main'} -- LSP UI
-    use 'jose-elias-alvarez/typescript.nvim' -- enchanced ts support
-    use 'onsails/lspkind.nvim' -- autocomplete icons
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
 
-  --formatting + linting
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'jayp0521/mason-null-ls.nvim'
-  
-  -- status line
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  
-  -- nvim tree
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
-  
-  -- Svelte Support
-  use 'othree/html5.vim'
-  use 'pangloss/vim-javascript'
-  use { 'evanleck/vim-svelte', branch = 'main'}
-  
-  -- Treesitter
-  use {
-      'nvim-treesitter/nvim-treesitter',
-      run = function()
-          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-          ts_update()
-      end,
-  }
-  
-  -- Commenting
-  use { 'numToStr/Comment.nvim' }
-  
-  -- Autopair tags + brackets
-  use { 'windwp/nvim-autopairs' }
-  use { 'windwp/nvim-ts-autotag', after="nvim-treesitter" } -- autoclose/pair html tags
-  
-  -- Buffer management 
-  use { 'romgrk/barbar.nvim' }
-  use {
-    "utilyre/barbecue.nvim",
-    requires = {
-      "neovim/nvim-lspconfig",
-      "smiteshp/nvim-navic",
-    },
-    config = function()
-      require("barbecue").setup()
-    end,
-  }
-  
-  -- Tmux + Window navigation
-  use "christoomey/vim-tmux-navigator"
-  use "szw/vim-maximizer"  
-  
-  -- Terminal
-  use {
-    "akinsho/toggleterm.nvim",
-    tag = '*', 
-  }
-    
-  -- fin.
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	-- LSP
+	use("williamboman/mason.nvim") --  manage + install
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig") -- configure lsp servers
+	use("hrsh7th/cmp-nvim-lsp") -- autocomplete lsp
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- LSP UI
+	use("jose-elias-alvarez/typescript.nvim") -- enchanced ts support
+	use("onsails/lspkind.nvim") -- autocomplete icons
+
+	--formatting + linting
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
+
+	-- status line
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+
+	-- nvim tree
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons", -- optional, for file icons
+		},
+		tag = "nightly", -- optional, updated every week. (see issue #1193)
+	})
+
+	-- Svelte Support
+	use("othree/html5.vim")
+	use("pangloss/vim-javascript")
+	use({ "evanleck/vim-svelte", branch = "main" })
+
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
+
+	-- Commenting
+	use({ "numToStr/Comment.nvim" })
+
+	-- Autopair tags + brackets
+	use({ "windwp/nvim-autopairs" })
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose/pair html tags
+
+	-- Buffer management
+	use({ "romgrk/barbar.nvim" })
+	use({
+		"utilyre/barbecue.nvim",
+		requires = {
+			"neovim/nvim-lspconfig",
+			"smiteshp/nvim-navic",
+		},
+		config = function()
+			require("barbecue").setup()
+		end,
+	})
+
+	-- Tmux + Window navigation
+	use("christoomey/vim-tmux-navigator")
+	use("szw/vim-maximizer")
+
+	-- Terminal
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+	})
+
+	-- fin.
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
--- nvim/lua/saysthetedd/plugins/autopairs.lua
+++ nvim/lua/saysthetedd/plugins/autopairs.lua
@@ -1,13 +1,27 @@
 local autopairs_setup, autopairs = pcall(require, "nvim-autopairs")
 if not autopairs_setup then
-  return
+	return
 end
 
 autopairs.setup({
-   check_ts = true,
-    ts_config = {
-        lua = {'string'},-- it will not add a pair on that treesitter node
-        javascript = {'template_string'},
-        java = false,-- don't check treesitter on java
-    }
+	check_ts = true,
+	ts_config = {
+		lua = { "string" }, -- it will not add a pair on that treesitter node
+		javascript = { "template_string" },
+		java = false, -- don't check treesitter on java
+	},
 })
+
+-- autocomplete + autopairs
+local cmp_autopairs_setup, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+if not cmp_autopairs_setup then
+	return
+end
+
+local cmp_setup, cmp = pcall(require, "cmp")
+if not cmp_setup then
+	return
+end
+
+-- make autopairs and completion work together
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
